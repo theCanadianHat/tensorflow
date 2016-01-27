d60a4dfc0bd247dc4404dd9f2fd3f47519b0cea7
root <jayssbronze@gmail.com>
2016-01-27 21:56:00 +0000

    add begining to Assignment 1

---
+++ assignments/a1/a1.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import munpy as mp
+import os
+import sys
+import tarfile
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle 
