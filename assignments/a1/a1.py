425dbb7403ffb6ab23a719f4a4cb923255a78c49
Noah Herron <jayssbronze@gmail.com>
2016-01-29 15:47:42 -0500

    added maybe_download function

--- assignments/a1/a1.py
+++ assignments/a1/a1.py
@@ -8,3 +8,19 @@ from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle 
+
+url = 'http://yaroslavvb.com/upload/notMNIST/'
+
+def maybe_download(filename, expected_bytes):
+	"""Download a file if not present, and make sure it's the right size"""
+	if not os.path.exists(filename);
+		filename, _ = urlretrieve(url + filename, filename)
+	statinfo = os.stat(filename)
+	if statinfo.st_size == expected_bytes:
+		print('Found and verified', filename)
+	else:
+		raise Exception('Failed to verify ' + filename + '. Can you get to it with a browser?')
+	return filename
+
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
